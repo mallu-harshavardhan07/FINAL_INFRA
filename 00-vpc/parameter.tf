5c0b57fcbc44b682a1ac4e24c2dfb770b1588cea
mallu-harshavardhan07 <malluharshavardhanreddy0705@gmail.com>
2025-10-26 15:56:02 +0530

    done

--- 00-vpc/parameter.tf
+++ 00-vpc/parameter.tf
@@ -4,19 +4,19 @@ resource "aws_ssm_parameter" "vpc_id" {
   value = module.vpc.vpc_id
 }
 
-resource "aws_ssm_parameter" "public_subnet_ids" {
+resource "aws_ssm_parameter" "public_subnet" {
   name  = "/roboshop/dev/public_subnet_ids"
   type  = "StringList"
   value = join(",", module.vpc.public_subnet_ids)
 }
 
-resource "aws_ssm_parameter" "private_subnet_ids" {
+resource "aws_ssm_parameter" "private_subnet" {
   name  = "/roboshop/dev/private_subnet_ids"
   type  = "StringList"
   value = join(",", module.vpc.private_subnet_ids)
 }
 
-resource "aws_ssm_parameter" "database_subnet_ids" {
+resource "aws_ssm_parameter" "database_subnet" {
   name  = "/roboshop/dev/database_subnet_ids"
   type  = "StringList"
   value = join(",", module.vpc.database_subnet_ids)
