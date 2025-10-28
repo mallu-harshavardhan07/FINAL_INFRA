1d3167647713f15c6b1b2250d831f5634beacaab
mallu-harshavardhan07 <malluharshavardhanreddy0705@gmail.com>
2025-10-28 11:03:38 +0530

    done

---
+++ 40-databases/bootstrap.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+component=$1
+dnf install ansible -y
+#ansible-pull -U https://github.com/daws-84s/ansible-roboshop-roles-tf.git -e component=$1 -e env=$2 main.yaml
+ansible-pull -U https://github.com/daws-84s/ansible-roboshop-roles-tf.git -e component=$1 -main.yaml
--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -7,3 +7,27 @@ resource "aws_instance" "mongodb" {
     Name = "roboshop-dev-monogdb"
   }
 }
+resource "terraform_data" "mongodb" {
+  triggers_replace = [
+    aws_instance.mongodb.id
+  ]
+  
+  provisioner "file" {
+    source      = "bootstrap.sh"
+    destination = "/tmp/bootstrap.sh"
+  }
+
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    password = "DevOps321"
+    host     = aws_instance.mongodb.private_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/bootstrap.sh",
+      "sudo sh /tmp/bootstrap.sh mongodb "
+    ]
+  }
+}
